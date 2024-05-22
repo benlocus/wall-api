b43646600c2d134e392b06bc4540b741669f7300
Ben Duke <benduke@Bens-MBP.localdomain>
2024-05-22 10:54:31 -0400

    feat: initial api test

--- main.ts
+++ main.ts
@@ -1,8 +1,28 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const books = new Map<string, any>();
+books.set("1", {
+  id: "1",
+  title: "The Hound of the Baskervilles",
+  author: "Conan Doyle, Arthur",
+});
+
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Hello world!";
+  })
+  .get("/book", (context) => {
+    context.response.body = Array.from(books.values());
+  })
+  .get("/book/:id", (context) => {
+    if (books.has(context?.params?.id)) {
+      context.response.body = books.get(context.params.id);
+    }
+  });
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
