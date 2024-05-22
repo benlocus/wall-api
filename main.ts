beb13a1ec12ad9c0f1cc1dce02b7358912523c88
Ben Duke <benduke@Bens-MBP.localdomain>
2024-05-22 15:55:02 -0400

    feat: visible responses!

--- main.ts
+++ main.ts
@@ -29,7 +29,7 @@ router.get("/", (context) => {
     response_type: "in_channel",
     text: response,
   };
-  context.response.body = JSON.stringify(response);
+  context.response.body = JSON.stringify(slackResponse);
 });
 
 const app = new Application();
